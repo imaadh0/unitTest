e8d74ef9c1327204dc9d59b31c52bbbbd720b17a
Imaadh Ifthikar <101471526+imaadh0@users.noreply.github.com>
2023-06-19 13:00:59 +0530

    Update SpringAzureDemoApplicationTests.java

--- src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
+++ src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
@@ -1,13 +1,21 @@
-package com.javatechie.azure.demo;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringAzureDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testMyService() {
+        // Test your component here
+        MyService myService = new MyService();
+        int result = myService.doSomeLogic(2, 3);
+        assertEquals(5, result);
+    }
 
+    @Test
+    void testAnotherComponent() {
+        // Test another component here
+        // ...
+    }
 }
